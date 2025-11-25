3958bef8cd85f9cb004c229b818219199ee838ea
arlopes-2006 <a.r.lopes@uni9.edu.br>
2025-11-25 20:27:23 -0300

    Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Configurações
+const precosPizzas = {
+  "CALABRESA": 43,
+  "MUSSARELA": 42,
+  "FRANGO COM CATUPIRY": 51,
+  "PORTUGUESA": 47,
+  "MARQUARITA": 43,
+  "QUATRO QUEIJOS": 55,
+  "VEGETARIANA": 49,
+  "PEPERONI": 52,
+  "NAPOLITANA": 43,
+  "CHOCOLATE": 45
+};
+
+const precoRefrigerante = 8;
+const precoSuco = 7;
+const pizzariaCoords = { lat: -23.653404, lng: -46.7121567 };
+
+// ... (conteúdo igual ao canvas; omitido por limite)
